c3aaa1113e91618b705f106caac977eb98f9f7d8
Gowtham S <raogowtham@gmail.com>
2026-01-02 14:17:48 +0530

    Revert "Prime Ng theme pack"
    This reverts commit 662817144e2de1953ff3829eb6c58a7f15f0f614.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
@@ -12,14 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-        options: {
-          darkModeSelector: false
-        }
-      }
-    })
+    provideHttpClient()
   ]
 };
